13157cbddad16a48c8bd7a5fbb4252f5c88df0a7
Max-Shevtsov <maxschew@yandex.ru>
2024-12-12 22:06:37 +0200

    add mappers

--- booking-flights-data/src/main/java/com/max/booking_flights_data/mappers/OfferToConcertsOffers.kt
+++ booking-flights-data/src/main/java/com/max/booking_flights_data/mappers/OfferToConcertsOffers.kt
@@ -1,12 +1,12 @@
 package com.max.booking_flights_data.mappers
 
-import com.max.booking_flights_data.api.models.OffersList
+import com.max.booking_flights_data.api.models.OffersListApi
 import com.max.booking_flights_data.api.models.PriceApi
 import com.max.booking_flights_domain.models.FlightToConcertOffer
 import com.max.booking_flights_domain.models.Price
 
-internal fun OffersList.toFlightToConcertOffer(): List<FlightToConcertOffer> {
-    return offers.map {
+internal fun OffersListApi.toFlightToConcertOffer(): List<FlightToConcertOffer> {
+    return offersApi.map {
         FlightToConcertOffer(
             id = it.id,
             title = it.title,
---
+++ booking-flights-data/src/main/java/com/max/booking_flights_data/mappers/TicketsListApiToTickets.kt
@@ -0,0 +1,52 @@
+package com.max.booking_flights_data.mappers
+
+import com.max.booking_flights_data.api.models.ArrivalApi
+import com.max.booking_flights_data.api.models.DepartureApi
+import com.max.booking_flights_data.api.models.LuggageApi
+import com.max.booking_flights_data.api.models.PriceApi
+import com.max.booking_flights_data.api.models.TicketsListApi
+import com.max.booking_flights_domain.models.Arrival
+import com.max.booking_flights_domain.models.Departure
+import com.max.booking_flights_domain.models.FlightToConcertOffer
+import com.max.booking_flights_domain.models.Luggage
+import com.max.booking_flights_domain.models.Price
+import com.max.booking_flights_domain.models.Ticket
+
+internal fun TicketsListApi.toTickets(): List<Ticket> {
+    return tickets.map {
+        Ticket(
+            id = it.id,
+            badge = it.badge,
+            price = it.price?.toPrice(),
+            providerName = it.providerName,
+            company = it.company,
+            departure = it.departureApi?.toDeparture(),
+            arrival = it.arrivalApi?.toArrival(),
+            hasTransfer = it.hasTransfer,
+            luggage = it.luggageApi?.toLuggage()
+        )
+    }
+}
+
+internal fun DepartureApi.toDeparture(): Departure {
+    return Departure(
+        town = town,
+        date = date,
+        airport = airport
+    )
+}
+
+internal fun ArrivalApi.toArrival(): Arrival {
+    return Arrival(
+        town = town,
+        date = date,
+        airport = airport
+    )
+}
+
+internal fun LuggageApi.toLuggage(): Luggage {
+    return Luggage(
+        hasLuggage = hasLuggage,
+        price = priceApi?.toPrice()
+    )
+}
