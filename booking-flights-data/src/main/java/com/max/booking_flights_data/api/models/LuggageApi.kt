1448d92684048c547673cb62499f7d9ae055d0e3
Max-Shevtsov <maxschew@yandex.ru>
2024-12-13 13:45:24 +0200

    change api modles

--- booking-flights-data/src/main/java/com/max/booking_flights_data/api/models/LuggageApi.kt
+++ booking-flights-data/src/main/java/com/max/booking_flights_data/api/models/LuggageApi.kt
@@ -2,7 +2,7 @@ package com.max.booking_flights_data.api.models
 
 import com.google.gson.annotations.SerializedName
 
-internal data class LuggageApi (
+data class LuggageApi (
 
     @SerializedName("has_luggage" ) var hasLuggage : Boolean? = null,
     @SerializedName("price"       ) var priceApi      : PriceApi?   = PriceApi()
